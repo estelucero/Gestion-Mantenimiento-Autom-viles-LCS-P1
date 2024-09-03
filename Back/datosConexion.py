f8c8bd89050ca58860f2731ff5e5d5e39b904d6f
estelucero <este.lucero2212@gmail.com>
2024-09-02 21:13:40 -0300

    Agrego login

--- Back/datosConexion.py
+++ Back/datosConexion.py
@@ -1,13 +1,13 @@
 
 class datosSQL(object):
   def __init__(self):
-    self.host = "COMPLETAR"
-    self.user = "COMPLETAR"
-    self.password = "COMPLETAR"
-    self.database = "COMPLETAR"
+    self.host = "b3psdfrcs2ajufhy7cov-mysql.services.clever-cloud.com"
+    self.user = "ujuznvazoa0ixtnl"
+    self.password = "XS3a0sxFPwZgVRr2wm8P"
+    self.database = "b3psdfrcs2ajufhy7cov"
 
 class datosAPI(object):
   def __init__(self):
-    self.ip = "COMPLETAR CON IP PROPIA"
-    self.puerto = "9090"
+    self.ip = "192.168.1.37"
+    self.puerto = 9090
 
